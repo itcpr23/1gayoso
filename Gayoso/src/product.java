daad2044e340445a51c37d2b02435f0893b6a6bc
jeemelgayoso <55048856+jeemelgayoso@users.noreply.github.com>
2019-09-23 03:21:23 -0400

    added "add product"

---
+++ Gayoso/src/product.java
@@ -0,0 +1,35 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class product {
+    String forname=  "com.mysql.jdbc.Driver";
+   String driver = "jdbc:mysql://localhost/bsit23";
+   String us = "root";
+   String ps1 ="";
+   String name;
+   public void add(String p,int qty,float pr){
+        try {
+            Class.forName(forname);
+      PreparedStatement ps = DriverManager.getConnection(driver,us,ps1).prepareStatement("insert into product values(null,?,?,?)");
+      ps.setString(1, p);
+      ps.setInt(2, qty);
+      ps.setFloat(3, pr);
+      ps.executeUpdate();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(product.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(product.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   }
+}
